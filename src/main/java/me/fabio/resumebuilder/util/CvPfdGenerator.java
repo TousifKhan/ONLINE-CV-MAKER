32870d4d3e4a7304ee6b5367cb8fbc2e2b69b6a9
Tousif Ahmed <tousifkhan510@gmail.com>
2019-12-01 23:12:17 +0530

    report generation

--- src/main/java/me/fabio/resumebuilder/util/CvPfdGenerator.java
+++ src/main/java/me/fabio/resumebuilder/util/CvPfdGenerator.java
@@ -13,7 +13,11 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import me.fabio.resumebuilder.entity.Education;
+import me.fabio.resumebuilder.entity.Experience;
+import me.fabio.resumebuilder.entity.Participation;
 import me.fabio.resumebuilder.entity.Personal;
+import me.fabio.resumebuilder.entity.Projects;
+import me.fabio.resumebuilder.entity.References;
 
 @Component
 public class CvPfdGenerator {
@@ -44,7 +48,27 @@ public class CvPfdGenerator {
             if(eduPara != null){
             	document.add(eduPara);
             }
+            
+            Paragraph expPara = getExperienceDetails(session.getExperience());
+            if(expPara != null){
+            	document.add(expPara);
+            }
+            
+            Paragraph partPara = getParticipationDetails(session.getParticipation());
+            if(partPara != null){
+            	document.add(partPara);
+            }
 
+            Paragraph proPara = getProjectDetails(session.getProject());
+            if(proPara != null){
+            	document.add(proPara);
+            }
+            
+            Paragraph refPara = getReferenceDetails(session.getReference());
+            if(refPara != null){
+            	document.add(refPara);
+            }
+            
             document.close();
 
         } catch (DocumentException ex) {
@@ -77,6 +101,65 @@ public class CvPfdGenerator {
 			para.add("Board / University: " + valueOf(e.getBoard())+"\n");
 			String marskAndTotal = valueOf(e.getObtainedMarks()) + " " + valueOf(e.getTotalMarks());
 			para.add("Marks / Total: " + marskAndTotal+"\n");
+			addEmptyLine(para, 4);
+		}
+		return para;
+	}
+	
+	private static Paragraph getExperienceDetails(Experience exp){
+		Paragraph para = null;
+		if(exp != null) {
+			para = new Paragraph();
+			para.add("Employer Name: " + valueOf(exp.getEmployer())+"\n");
+			para.add("Designation: " + valueOf(exp.getDesignation())+"\n");
+			String dates = valueOf(exp.getJoiningDate()) + " / "+ valueOf(exp.getLeavingDate());
+			para.add("Joining Date / Leaving Date: " + dates +"\n");
+			String workingExperience = valueOf(exp.getWorking()) + " / "+ valueOf(exp.getYearOfExp());
+			para.add("Working / Experience: " + workingExperience +"\n");
+			addEmptyLine(para, 4);
+		}
+		return para;
+	}
+	
+	private static Paragraph getParticipationDetails(Participation part){
+		Paragraph para = null;
+		if(part != null) {
+			para = new Paragraph();
+			para.add("Participation Name: " + valueOf(part.getPartName())+"\n");
+			para.add("Sponsored by: " + valueOf(part.getSponsores())+"\n");
+			para.add("Note: " + valueOf(part.getNote())+"\n");
+			para.add("Topic: " + valueOf(part.getTopic())+"\n");
+			para.add("Place: " + valueOf(part.getPlace())+"\n");
+			addEmptyLine(para, 4);
+		}
+		return para;
+	}
+	
+	private static Paragraph getProjectDetails(Projects pro){
+		Paragraph para = null;
+		if(pro != null) {
+			para = new Paragraph();
+			para.add("Project Title: " + valueOf(pro.getTitle())+"\n");
+			para.add("Project Guide: " + valueOf(pro.getGuide())+"\n");
+			String frontBackEnd = valueOf(pro.getFrontEnd()) + " / "+ valueOf(pro.getBackEnd());
+			para.add("Front End / Back End: " + frontBackEnd +"\n");
+			String inputOutput = valueOf(pro.getInputs()) + " / "+ valueOf(pro.getOutputs());
+			para.add("Input / Output: " + inputOutput +"\n");
+			para.add("Description: " + valueOf(pro.getDescription())+"\n");
+			addEmptyLine(para, 4);
+		}
+		return para;
+	}
+	
+	private static Paragraph getReferenceDetails(References ref){
+		Paragraph para = null;
+		if(ref != null) {
+			para = new Paragraph();
+			para.add("Reference Name: " + valueOf(ref.getName())+"\n");
+			para.add("Designation: " + valueOf(ref.getDesignation())+"\n");
+			para.add("Affiliation: " + valueOf(ref.getAffiliation())+"\n");
+			para.add("Email: " + valueOf(ref.getEmailId())+"\n");
+			para.add("Contact No#: " + valueOf(ref.getContactNo())+"\n");
 			addEmptyLine(para, 4);
 		}
 		return para;
